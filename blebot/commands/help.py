aad746f36bb8f68a160caeea6573f9bea2f0b50c
Chris Lee <sihrc.c.lee@gmail.com>
2016-04-19 14:58:38 -0400

    FIX: Help command

--- blebot/commands/help.py
+++ blebot/commands/help.py
@@ -1,5 +1,8 @@
 from .config import DEFAULT_MESSAGE, COMMANDS, ALLOWED
 
+def handle_help():
+    return [], "\nAvailable commands are:\n{commands}\n\nYou can also try:\n`/help [command]`".format(commands=ALLOWED())
+
 def handle_action(command, action, args, message, role=None):
     if not action:
         return [], "\nAvailable commands are:\n{commands}\n\nYou can also try:\n`/help [command]`".format(commands=ALLOWED())
